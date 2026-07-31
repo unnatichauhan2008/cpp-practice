42faf4f810ea61d4be6f4b37165e9b06cffcec40
Your GitHub Username <unnatichauhan2008@gmail.com>
2026-07-31 13:10:55 +0530

    Added average program

---
+++ average.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int num1, num2, num3;
+cout<<"Enter the value of num1: ";
+cin>>num1;
+
+cout<<"Enter the value of num2: ";
+cin>>num2;
+
+cout<<"Enter the value of num3: ";
+cin>>num3;
+ 
+int AVERAGE= (num1+num2+num3)/3;
+cout<<"The average is: "<<AVERAGE;
+
+return 0;
+
+
+
+
+}
