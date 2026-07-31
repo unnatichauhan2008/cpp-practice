af0402b8ca5431e3c1449e8772c816b7d7f08adf
Your GitHub Username <unnatichauhan2008@gmail.com>
2026-07-31 13:37:18 +0530

    Added typeofnumber program

---
+++ typeofnumber.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+float num;
+cout<<"Enter the value of num:";
+cin>>num;
+
+if(num>0){
+    cout<<"The num is a positive one.";
+}
+
+else if(num<0){
+cout<<"The num is a negative one.";
+}
+
+else{
+cout<<"The num is zero";
+}
+return 0;
+
+
+
+
+
+}
