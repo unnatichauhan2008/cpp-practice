853a6233709f78344c340d4df8be8061a305ac94
Your GitHub Username <unnatichauhan2008@gmail.com>
2026-08-06 22:01:45 +0530

    Added bankaccount Programme.

--- demo.cpp
+++ demo.cpp
@@ -1,19 +1,81 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-float moneyrecieved(int currentmoney, float rate=1.05){
+class bankaccount
+{
 
-    return currentmoney*rate;
-}
-int main(){
-int money;
-cout<<"Enter the amount of currency:";
-cin>>money;
-cout<<"If you have"<<money<<" in your pocket then you would recieve: "<<moneyrecieved(money)<<" after 1 year.";
+private:
+    float balance;
+    string holder_name;
+    int acc_number;
+
+public:
+    void createaccount()
+    {
+        cout << "Enter your name: ";
+        cin >> holder_name;
+
+        cout << "Enter your account number: ";
+        cin >> acc_number;
+
+        cout << "Enter your balance amount: ";
+        cin >> balance;
+    }
+
+    void depositMoney()
+    {
+        float amount;
+        cout << "Enter amount to deposit: " << endl;
+        cin >> amount;
+        balance = balance + amount;
+        cout << "The new balance amount is: " << balance << endl;
+    }
 
 
-return 0;
+    void showBalance(){
+        cout<<"The current balance is: "<<balance<<endl;
+    }
 
+    void withdrawMoney()
+    {
+        int withdraw;
 
+        cout << "Enter amount to withdraw: ";
+        cin >> withdraw;
+       
+        if (withdraw <= balance)
+        {
+             balance = balance - withdraw;
+            cout << "Amount can be withdrawen." << endl;
+            cout<<"Remaining Balance: "<<balance<<endl;
+        }
+        else
+        {
+            cout << "Insufficient balance" << endl;
+        }
+    }
 
+    
+    void displayAccount()
+    {
+
+        cout << "\n----------ACCOUNT DETAILS---------\n";
+
+        cout << "NAME: " << holder_name << endl;
+        cout << "ACCOUNT NUMBER: " << acc_number << endl;
+        cout << "BALANCE REMAINING: " << balance << endl;
+    }
+};
+
+int main()
+{
+    bankaccount acc;
+    acc.createaccount();
+    acc.depositMoney();
+    acc.showBalance();
+    acc.withdrawMoney();
+    acc.displayAccount();
+
+    return 0;
 }
--- studentreportcard.cpp
+++ studentreportcard.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+#include <iostream>
+#include<string>
+
 using namespace std;
 
 class Students
 {
-    public:
+public:
     string Name;
     int Roll_no;
     int studentclass;
@@ -11,102 +13,87 @@ class Students
     void readData()
     {
 
-        cout<<"Enter student name: "<<endl;
-        cin>>Name;
+        cout << "Enter student name: " << endl;
+        cin >> Name;
 
-        cout<<"Enter Roll Number: "<<endl;
-        cin>>Roll_no;
+        cout << "Enter Roll Number: " << endl;
+        cin >> Roll_no;
 
-        cout<<"Enter your class: "<<endl;
-        cin>>studentclass;
+        cout << "Enter your class: " << endl;
+        cin >> studentclass;
 
-        cout<<endl;
+        cout << endl;
 
-        cout<<"ENTER YOUR MARKS IN 5 SUBJECTS: "<<endl;
+        cout << "ENTER YOUR MARKS IN 5 SUBJECTS: " << endl;
 
-        cout<<"English: "<<endl;
-        cin>>marks[0];
+        cout << "English: " << endl;
+        cin >> marks[0];
 
-        cout<<"Maths: "<<endl;
-        cin>>marks[1];
+        cout << "Maths: " << endl;
+        cin >> marks[1];
 
-        cout<<"Science: "<<endl;
-        cin>>marks[2];
+        cout << "Science: " << endl;
+        cin >> marks[2];
 
-        cout<<"Social Science: "<<endl;
-        cin>>marks[3];
+        cout << "Social Science: " << endl;
+        cin >> marks[3];
 
-        cout<<"Hindi: "<<endl;
-        cin>>marks[4];
-
-
+        cout << "Hindi: " << endl;
+        cin >> marks[4];
     }
 
     void calculatepercentage()
     {
+
         int total;
-        total= marks[0]+marks[1]+marks[2]+marks[3]+marks[4];
-        percentage= total/5.0;
+        total = marks[0] + marks[1] + marks[2] + marks[3] + marks[4];
+        percentage = total / 5;
 
-    if (percentage >=90)
-    grade= 'A';
-else if(percentage>=85)
-grade= 'B';
-else if(percentage>=65)
-grade= 'C';
-else if(percentage>=50)
-grade= 'D';
-else
-grade= 'F';
+        if (percentage >= 90)
+            grade = 'A';
+        else if (percentage >= 85)
+            grade = 'B';
+        else if (percentage >= 65)
+            grade = 'C';
+        else if (percentage >= 50)
+            grade = 'D';
+        else
+            grade = 'F';
+    }
+    void displayreport()
+    {
 
+        cout << endl;
+        cout << "-----------REPORT CARD-----------" << endl;
+        cout << "Name: " << Name << endl;
+        cout << "Roll Number: " << Roll_no << endl;
+        cout << "Class: " << studentclass << endl;
 
+        cout << endl;
+        cout << "---------------MARKS--------------" << endl;
+        cout << "ENGLISH: " << marks[0] << endl;
+        cout << "MATHS: " << marks[1] << endl;
+        cout << "SCIENCE: " << marks[2] << endl;
+        cout << "SOCIAL SCIENCE: " << marks[3] << endl;
+        cout << "HINDI: " << marks[4] << endl;
+
+        cout << "Percentage:" << percentage << "%" << endl;
+        cout << "Grade: " << grade << endl;
     }
 
-    void displayreport(){
-        cout<<endl;
-        cout<<"-----------REPORT CARD-----------"<<endl;
-     cout<<"Name: "<<Name<<endl;
-     cout<<"Roll Number: "<<Roll_no<<endl;
-     cout<<"Class: "<<studentclass<<endl;
-
-     cout<<endl;
-     cout<<"---------------MARKS--------------"<<endl;
-     cout<<"ENGLISH: "<<marks[0]<<endl;
-     cout<<"MATHS: "<<marks[1]<<endl;
-     cout<<"SCIENCE: "<<marks[2]<<endl;
-     cout<<"SOCIAL SCIENCE: "<<marks[3]<<endl;
-     cout<<"HINDI: "<<marks[4]<<endl;
-
-     cout<<"Percentage:"<<percentage<<"%"<<endl;
-     cout<<"Grade: "<<grade<<endl;
-    }
-
-
-
-
-    private:
+private:
     int marks[5];
-    float percentage;
+    int percentage;
     char grade;
-
 };
 
-int main(){
+int main()
+{
 
-Students Harsh;
-Harsh.readData();
-Harsh.calculatepercentage();
-Harsh.displayreport();
-
-
-
-
-
-
-
-
-
-
+    Students Harsh;
+    Harsh.readData();
+    Harsh.calculatepercentage();
+    Harsh.displayreport();
 
     return 0;
 }
